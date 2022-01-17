723b39f5caaa0e7696281a56ee2ca0b848c8c91d
dikranteymur <mdteymur@gmail.com>
2022-01-17 11:35:27 +0300

    PickerPOC fixed

--- 3.PickerPOC/3.PickerPOC WatchKit Extension/InterfaceController.swift
+++ 3.PickerPOC/3.PickerPOC WatchKit Extension/InterfaceController.swift
@@ -10,17 +10,64 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var timerButton: WKInterfaceButton!
+    @IBOutlet weak var weightPicker: WKInterfacePicker!
+    @IBOutlet weak var temperatureLabel: WKInterfaceLabel!
+    @IBOutlet weak var temperaturePicker: WKInterfacePicker!
+    
+    var ounces = 16
+    var cookTemp = MeatTemperature.medium
+    var timerRunning = false
+    var weightItems: [WKPickerItem] = []
+    var tempItems: [WKPickerItem] = []
 
     override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        super.awake(withContext: context)
+        updateConfiguration()
+        for i in 1...32 {
+            let item = WKPickerItem()
+            item.title = String(i)
+            weightItems.append(item)
+        }
+        weightPicker.setItems(weightItems)
+        weightPicker.setSelectedItemIndex(ounces - 1)
+        
+        for i in 1...4 {
+            let item = WKPickerItem()
+            item.contentImage = WKImage(imageName: "temp-\(i)")
+            tempItems.append(item)
+        }
+        temperaturePicker.setItems(tempItems)
+        onTemperatureChanged(0)
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+    func updateConfiguration() {
+        
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+    @IBAction func onTimerButton() {
+        if timerRunning {
+            timer.stop()
+            timerButton.setTitle("Start Timer")
+        } else {
+            let time = cookTemp.cookTimeForOunces(ounces)
+            timer.setDate(Date(timeIntervalSinceNow: time))
+            timer.start()
+            timerButton.setTitle("Stop Timer")
+        }
+        timerRunning.toggle()
     }
-
+    
+    @IBAction func onWeightChanged(_ value: Int) {
+        ounces = value + 1
+    }
+    
+    @IBAction func onTemperatureChanged(_ value: Int) {
+        let temp = MeatTemperature(rawValue: value)!
+        cookTemp = temp
+        temperatureLabel.setText(temp.stringValue)
+    }
+    
 }
--- 3.PickerPOC/Models/MeatTemperature.swift
+++ 3.PickerPOC/Models/MeatTemperature.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum MeatTemperature: Int {
+    case rare = 0, mediumRate, medium, wellDone
+    
+    var stringValue: String {
+        let temperature = ["Rare", "Medium Rate", "Medium", "Well Done"]
+        return temperature[self.rawValue]
+    }
+    
+    var timeModifier: Double {
+        let modifiers = [0.5, 0.75, 1.0, 1.5]
+        return modifiers[self.rawValue]
+    }
+    
+    func cookTimeForOunces(_ ounces: Int) -> TimeInterval {
+        let baseTime: TimeInterval = 47 * 60
+        let baseWeight = 16
+        let weightModifier: Double = Double(ounces) / Double(baseWeight)
+        let tempModifier = self.timeModifier
+        return baseTime * weightModifier * tempModifier
+    }
+}
